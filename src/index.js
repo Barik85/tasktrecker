db1c29e59709318621868a5f2d2b6649e92224ae
Apelsin16 <apelsin@i.ua>
2018-11-18 11:12:39 +0200

    styles fixed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import WebFont from 'webfontloader'
 import store from './redux/store';
 import './index.css';
 import App from './components/App';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:400,500', 'sans-serif']
+  }
+});
 
 ReactDOM.render(
   <Provider store={store}>
