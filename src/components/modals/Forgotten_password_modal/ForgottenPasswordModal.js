6f99f02ff2e39bd53a68dde30f8e8b3dd3a7984f
Apelsin16 <apelsin@i.ua>
2019-06-02 11:11:56 +0300

    layout ready

--- src/components/modals/Forgotten_password_modal/ForgottenPasswordModal.js
+++ src/components/modals/Forgotten_password_modal/ForgottenPasswordModal.js
@@ -76,58 +76,53 @@ export default class ForgottenPasswordModal extends Component {
           <button onClick={closeModal} className={styles.close}>
             <Close className={styles.closesvg} />
           </button>
-          <div className={styles.left}>
-            <h3 className={styles.title}>Восстановление пароля</h3>
-              <form action="">
-                <div className={styles.login_inputEmail}>
-                  <p>Введите e-mail для восстановления пароля</p>
-                  <label htmlFor="email">
-                    <span>
-                      <img src={letterIcon} alt="letter" width="5%" />
-                    </span>
-                  </label>
-                  <input
-                    className={styles.login_box}
-                    type="email"
-                    name="email"
-                    placeholder="Почта"
-                    onChange={this.handleInputChange}
-                    value={this.state.email}
-                  />
-                </div>
-                <button className={styles.button}>Получить временный пароль</button>
-              </form>
-              <Link to="/login" className={styles.login_link} onClick={this.handleClose}>Войти </Link>
-            </div> 
-          <div className={styles.right}>
-            <h4 className={styles.title}>Войти как пользователь</h4>
-                       
-                
-                  <div className={styles.socialnet_box}>
-                    <div>
-                      <button type="button" className={styles.btn}>
-                        <img src={facebookLogo} alt="facebook_logo" className={styles.btn_img} />
-                        Facebook
-                      </button>
-                    </div>
-                    <div>
-                      <a href="https://taskboard.luisi.top/auth/google" className={styles.btn}>
-                        <img src={googleplusLogo} alt="google_logo" className={styles.btn_img_google} />
-                        Google
-                      </a>
-                    </div>
-                    <div>
-                      <button type="button" className={styles.btn}>
-                        <img src={linkedinLogo} alt="linked_in_logo" className={styles.btn_img} />
-                        Linked In
-                      </button>
-                    </div>
-                  </div>
-                  <Link to="/register" className={styles.register_link} onClick={this.handleClose}>Зарегистрироваться </Link>
-                </div>
-              
-          </div>
-        </Modal>
+          <h3 className={styles.title}>Восстановление пароля</h3>
+          <form action="">
+            <div className={styles.login_inputEmail}>
+              <p>Введите e-mail для восстановления пароля</p>
+              <label htmlFor="email">
+                <span>
+                  <img src={letterIcon} alt="letter" width="5%" />
+                </span>
+              </label>
+              <input
+                className={styles.login_box}
+                type="email"
+                name="email"
+                placeholder="Почта"
+                onChange={this.handleInputChange}
+                value={this.state.email}
+              />
+            </div>
+            <button className={styles.button}>Получить временный пароль</button>
+          </form>
+          <fieldset className={styles.fieldset}>
+            <legend className={styles.text}>или</legend>
+            <div className={styles.socialnet_box}>
+              <div>
+                <button type="button" className={styles.btn}>
+                  <img src={facebookLogo} alt="facebook_logo" className={styles.btn_img} />
+                  Facebook
+                </button>
+              </div>
+              <div>
+                <a href="https://taskboard.luisi.top/auth/google" className={styles.btn}>
+                  <img src={googleplusLogo} alt="google_logo" className={styles.btn_img_google} />
+                  Google
+                </a>
+              </div>
+              <div>
+                <button type="button" className={styles.btn}>
+                  <img src={linkedinLogo} alt="linked_in_logo" className={styles.btn_img} />
+                  Linked In
+                </button>
+              </div>
+            </div>
+            <Link to="/register" className={styles.register_link} onClick={this.handleClose}>Зарегистрироваться </Link>
+            <Link to="/login" className={styles.login_link} onClick={this.handleClose}>Войти </Link>
+          </fieldset>
+        </div>
+      </Modal>
     );
   }
 }
