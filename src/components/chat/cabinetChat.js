4b0a39466fad3aa6adeb8b8f6811f07ac983f4bf
anatoliianatolich <anatoliianatolich@gmail.com>
2019-07-22 23:11:21 +0300

    add style

--- src/components/chat/cabinetChat.js
+++ src/components/chat/cabinetChat.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import styles from './chat.module.scss';
 // import openSocket from 'socket.io-client';
 import { v4 } from 'uuid';
+import icon1 from "../../images/user1.png";
 
 
 // const socket = openSocket('http://localhost:5050/');
@@ -25,11 +26,15 @@ export default class CabinetChat extends Component {
 
   addMessage = name => e =>  {
     e.preventDefault();
-    let a = this.state.arrMessage.concat({id:v4(), name:name ,message:this.state.message})
-    console.log(a);
-    this.setState(
-      {arrMessage: a },
-      ()=> {console.log(this.state.arrMessage)}
+
+    this.setState( prevState =>({
+    arrMessage: prevState.arrMessage.concat({id:v4(), name:name ,message:prevState.message})
+    }) ,
+    ()=> {
+      console.log(this.state.message);
+    }
+    )
+    this.setState(prewState => ({message: ''})
     )
     // console.log(this.state.arrMessage);
     //socket.on('newMessage', )
@@ -58,35 +63,23 @@ export default class CabinetChat extends Component {
     return (
       <div className={valVisible} >
         <button className={iconClose} onClick={this.closeChat}>x</button>
-        {/*{console.log('61', arrMessage.length)}*/}
-          {this.state.arrMessage.length > 0 &&
+        {this.state.arrMessage.length > 0 &&
           <div>
             <ul>
               {arrMessage.map( (i) => (
                 <li key={i.id} className={styles.sendUser}>
-                  {i.message}
-                  {console.log(i.message)}
+                  <div className={styles.userData}>
+                    <img src={icon1} alt="avatar"/>
+                    <p>{i.name}</p>
+                  </div>
+                  <div className={styles.userMessage}>{i.message}</div>
                 </li>
                 )
-               ) }
+              ) }
             </ul>
-          {/*<span>{this.state.arrMessage.length}</span>*/}
           </div>
 
           }
-
-          {/*//   <ul className={styles.blockMessage}>*/}
-          {/*//   {arrMessage.map(elem => {*/}
-          {/*//     <li key={elem.id}>*/}
-          {/*//       <img src="/layout/tasktreker/src/images/user1.png" alt="avatar"/>*/}
-          {/*//       <p>{elem.name}</p>*/}
-          {/*//       <p>{elem.message}</p>*/}
-          {/*//     </li>*/}
-          {/*//     })*/}
-          {/*//   }*/}
-          {/*// </ul>*/}
-          {/*}*/}
-
         <section className={styles.blockIntroduce}>
           <input type="text" value={this.state.message} onChange={this.handlerChange} />
           <button onClick={this.addMessage(name)}>send</button>
